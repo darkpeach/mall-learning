ade876393319d42f11fd2e6170a033a5c22e3669
Henry_Z <zhr900301@gmail.com>
2020-07-22 14:18:08 -0700

    4) integrate with JWT and spring security

--- src/main/java/com/xiaoyang/mall/config/MyBatisConfig.java
+++ src/main/java/com/xiaoyang/mall/config/MyBatisConfig.java
@@ -8,6 +8,6 @@ import org.springframework.context.annotation.Configuration;
  * Created by HZ on 2020/07/08.
  */
 @Configuration
-@MapperScan("com.xiaoyang.mall.mbg.mapper")
+@MapperScan({"com.xiaoyang.mall.mbg.mapper","com.xiaoyang.mall.dao"})
 public class MyBatisConfig {
 }
---
+++ src/main/java/com/xiaoyang/mall/dao/UmsAdminRoleRelationDao.java
@@ -0,0 +1,19 @@
+package com.xiaoyang.mall.dao;
+
+import com.xiaoyang.mall.mbg.model.UmsPermission;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 后台用户与角色管理自定义Dao
+ * Created by HZ on 2020/07/15.
+ */
+
+public interface UmsAdminRoleRelationDao {
+
+    /**
+     * 获取用户所有权限(包括+-权限)
+     */
+    List<UmsPermission> getPermissionList(@Param("adminId") Long adminId);
+}
